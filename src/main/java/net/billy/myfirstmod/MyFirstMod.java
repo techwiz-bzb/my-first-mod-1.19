19dfee00cbf8f3337005e15fe91749d4bb0143ad
BIg Betty V2 <billybrudenell@gmail.com>
2022-11-01 21:25:46 +0000

    new comment

---
+++ src/main/java/net/billy/myfirstmod/MyFirstMod.java
@@ -0,0 +1,14 @@
+package net.billy.myfirstmod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MyFirstMod implements ModInitializer {
+	public static final String MOD_ID = "myfirstmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/billy/myfirstmod/MyFirstModClient.java
@@ -0,0 +1,10 @@
+package net.billy.myfirstmod;
+
+import net.fabricmc.api.ClientModInitializer;
+// comment
+public class MyFirstModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
